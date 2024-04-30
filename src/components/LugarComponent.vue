bd7d2d55d68027311b7decdf609c8cfabf86ad0c
Kevin Cuesta <CRACKESAN@GMAIL.COM>
2024-04-30 07:00:18 -0500

    Creacion componentes y vistas

---
+++ src/components/LugarComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <ion-input :id="id" :value="value" :placeholder="placeholder" :clear-input="clearInput" :debounce="debounce" :disabled="disabled" :maxlength="maxlength" :min="min" :max="max" :step="step"></ion-input>
+  </template>
+  
+  <script lang="ts">
+  import { IonInput } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    components: { IonInput },
+    props: {
+      id: {
+        type: String,
+        required: true
+      },
+      value: String,
+      placeholder: String,
+      clearInput: Boolean,
+      debounce: Number,
+      disabled: Boolean,
+      maxlength: Number,
+      min: Number,
+      max: Number,
+      step: Number
+    }
+  });
+  </script>
+  
+  <style scoped>
+  /* Estilos del componente */
+  /* Estilos globales */
+  </style>
+  
---
+++ src/views/PaisPage.vue
@@ -0,0 +1,30 @@
+    <template>
+        <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+            <ion-title>Pais</ion-title>
+            </ion-toolbar>
+        </ion-header>  
+        <ion-content>       
+            <div id="login-container">  
+            <div id="login-form">
+                <LugarComponent id="id" name="id" type="text"/>
+                <LugarComponent id="codigo" name="codigo" label="Código Dane: " />
+                <LugarComponent id="nombre" name="nombre" label="Nombre: " />
+            </div>
+            <div> 
+                <CrudButtonComponent />
+            </div>         
+            </div>
+        </ion-content>
+        </ion-page>
+    </template>
+    
+    <script setup lang="ts">
+    import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+    import LugarComponent from '@/components/LugarComponent.vue';
+    import CrudButtonComponent from '@/components/CrudButtonComponent.vue';
+    </script>
+    
+    <style scoped src="../theme/container.css"></style>
+    
